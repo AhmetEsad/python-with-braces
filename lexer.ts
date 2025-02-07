313b65d28f0489d549042a96165059f89b4a879b
ahmet <ahmet@example.com>
2025-02-07 16:49:51 +0300

    add dict set string support and cover edge cases. still a lot missing

--- lexer.ts
+++ lexer.ts
@@ -7,6 +7,7 @@ type Config = {
 	lineSeparator: string
 }
 
+import { INSPECT_MAX_BYTES } from 'buffer';
 import fs from 'fs';
 import path from 'path';
 
@@ -16,22 +17,103 @@ export function processFile(content: string, line_separator: string, tab_char: s
 	let val = '';
 	let actual_length = 0;
 	let lines: string[] = [];
+
+	let IF = 'if';
+	let DEF = 'def';
+	let ELSE = 'else';
+	let ELIF = 'elif';
+
+	let token = '';
+
+	let in_if = false;
+	let in_def = false;
+
+	let in_singleq_string = false;
+	let in_doubleq_string = false;
+	let in_tripleq_string = false; // TODO add """/''' support
+	let in_string = false;
+
+	let ignore_next_char = false;
+
+	let was_viable = false;
+	let is_viable = false;
+
+	content.endsWith(line_separator) || (content += line_separator);
+
 	content.split('').forEach((c, i) => {
+		// console.log(token);
 		switch (c) {
+			case ':':
+			case ' ':
+			case '\t':
+			case '	':
+				if (!in_string) {
+					if ([IF, DEF, ELSE, ELIF].includes(token)) {
+						is_viable = true;
+					}
+
+					token = '';
+				}
+				val += c;
+			break;
+
+			case '\\':
+				ignore_next_char = true;
+				break;
+
+			case '"':
+				if (!ignore_next_char) in_doubleq_string = !in_doubleq_string; else ignore_next_char = false;
+				val += c;
+
+				in_string = in_doubleq_string || in_singleq_string || in_tripleq_string;
+
+				if (in_string) {
+					if(is_viable) was_viable = true;
+					is_viable = false;
+				} else {
+					is_viable = was_viable;
+					was_viable = false;
+				}
+				break;
+			case "'":
+				if (!ignore_next_char) in_singleq_string = !in_singleq_string; else ignore_next_char = false;
+				val += c;
+
+				in_string = in_doubleq_string || in_singleq_string || in_tripleq_string;
+
+				if (in_string) {
+					if(is_viable) was_viable = true;
+					is_viable = false;
+				} else {
+					is_viable = was_viable;
+					was_viable = false;
+				}
+				break;
+
 			case '{':
+				if (!is_viable) {
+					val += c;
+					break;
+				}
 				awaiting_newline.push(1)
 				break;
 
 			case '}':
+				if (!is_viable) {
+					val += c;
+					break;
+				}
 				// awaiting_newline.push(-1);
 				level--;
 				actual_length++;
+				if (level === 0) (is_viable = false);
 				break;
 
 			case line_separator:
+				token = '';
 				let trimmed = val.trim();
 
-				if(val.length > 0 || actual_length === 0) lines.push(tab_char.repeat(level) + trimmed);
+				if (trimmed.length > 0 || actual_length === 0) lines.push(tab_char.repeat(level) + trimmed);
 				val = '';
 				actual_length = 0;
 				awaiting_newline.forEach(n => {
@@ -44,6 +126,7 @@ export function processFile(content: string, line_separator: string, tab_char: s
 				val += c;
 				break;
 		}
+		if (Array.from(new Set((IF + DEF + ELSE + ELIF).split(''))).includes(c) && !in_string) token += c;
 	});
 
 	return lines.join('\n');
